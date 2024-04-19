f87460ab7f60a8aef362c2f3fed7dcc1d9665f28
ZAREK ALEXIS NAZAR DUTKA <47696115@ort>
2024-04-19 12:12:13 -0300

    un poquito mas todavia

--- src/controllers/provincia-controller.js
+++ src/controllers/provincia-controller.js
@@ -9,6 +9,15 @@ const provinciaService = new Provincias();
 //nota: aparentemente hay que validar TODO
 provinciaController.get("/", (req, res) => {
 
+  id = provinciaService.ChequearProvinciaExiste(req.params.id)
+  if (id) {
+    let province = provinciaService.SearchProvinceById(id) //funcion que crea provincias
+    return res.json(province)
+    
+  }  else {
+    let provinces = provinciaService.SearchProvinces(id) //funcion que crea provincias
+    return res.json(provinces)
+  }
 
 })
 provinciaController.post("/", (req, res) => { // QUEDA SOLUCIONAR EL CHEQUEO DE DATOS
@@ -51,11 +60,58 @@ let latitude = Number(req.body.latitude)
 
 
 })
-provinciaController.patch("/", (req, res) => {
+provinciaController.patch("/:id", (req, res) => {
+  let error = false
+  let name =
+    typeof req.body.name  == "string"
+      ? req.body.name 
+      : (error = true);
+      console.log("name" + error)
+  let full_name =
+    typeof req.body.full_name == "string" ? req.body.full_name : (error = true);
+let latitude = Number(req.body.latitude)
+     latitude =
+    latitude == 'null'
+      ? req.body.latitude
+      : (error = true);
+      console.log("user" + error)
+  let longitude = Number(req.body.longitude)
+   longitude =
+    longitude == 'null' 
+      ? longitude
+      : (error = true);
+      console.log("att" + error)
+      let dO = Number(req.body.display_order)
+      dO =
+        dO == 'null' 
+         ? dO
+         : (error = true); 
+         console.log("rat" + error)
+  if (error == false) {
+    let province = provinciaService.EditarPorId(name,full_name,latitude,longitude,dO,req.params.id) //funcion que crea provincias
+    return res.json(province)
+    
+  }  else {
+    return res.status(400).send({
+
+      reason: "Datos no validos",
+    });
+  }
 
 
 })
-provinciaController.delete("/", (req, res) => {
+provinciaController.delete("/:id", (req, res) => {
+ id = provinciaService.ChequearProvinciaExiste(req.params.id)
+  if (id) {
+    let province = provinciaService.DeleteProvince(id) //funcion que crea provincias
+    return res.json(province)
+    
+  }  else {
+    return res.status(400).send({
+
+      reason: "Datos no validos",
+    });
+  }
 
 
 })
--- src/service/provincia-service.js
+++ src/service/provincia-service.js
@@ -1,15 +1,77 @@
 export default class Provincias {
-    CreateProvince(name,full_name,latitude,longitude,dO){
-
-        //la super query vendria aca en vez de esto:
-        let datoshardcodeados = {
-            "name":name,
-            "full_name":full_name,
-            "latitude":latitude,
-            "longitude":longitude,
-            "dO":dO
-        }
-        return datoshardcodeados
-    }
+  CreateProvince(name, full_name, latitude, longitude, dO) {
+    //la super query vendria aca en vez de esto:
+    let datoshardcodeados = {
+      name: name,
+      full_name: full_name,
+      latitude: latitude,
+      longitude: longitude,
+      dO: dO,
+    };
+    return datoshardcodeados;
   }
-  
+  SearchProvincia() {
+    //query
+    let datoshardcodeados = [
+      {
+        name: "BSAS",
+        full_name: "Buenos Aires",
+        latitude: 14,
+        longitude: 214,
+        dO: 1,
+      },
+      {
+        name: "RSR",
+        full_name: "Rosario",
+        latitude: 14,
+        longitude: 214,
+        dO: 1,
+      },
+    ];
+    return datoshardcodeados;
+  }
+  SearchProvinciaById(id) {
+    //query
+    let datoshardcodeados = [
+      {
+        name: "BSAS",
+        full_name: "Buenos Aires",
+        latitude: 14,
+        longitude: 214,
+        dO: 1,
+      },
+      {
+        name: "RSR",
+        full_name: "Rosario",
+        latitude: 14,
+        longitude: 214,
+        dO: 1,
+      },
+    ];
+    return datoshardcodeados;
+  }
+  DeleteProvincia(id) {
+    //query
+    let datoshardcodeados = [
+      {
+        name: "BSAS",
+        full_name: "Buenos Aires",
+        latitude: 14,
+        longitude: 214,
+        dO: 1,
+      },
+    ];
+    return datoshardcodeados;
+  }
+  EditarPorId(name, full_name, latitude, longitude, dO,id) {
+    //la super query vendria aca en vez de esto:
+    let datoshardcodeados = {
+      name: name,
+      full_name: full_name,
+      latitude: latitude,
+      longitude: longitude,
+      dO: dO,
+    };
+    return datoshardcodeados;
+  }
+}
